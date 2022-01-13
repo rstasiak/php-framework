d87f216d130f95a311d55521153503ee5a4ec958
Radek Stasiak <rad@MacBook-Air-Radek-2.local>
2022-01-13 14:09:51 +0100

    json controller

---
+++ src/JsonController.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace PHPFramework;
+
+
+use DI\Container;
+use Twig\Environment;
+use PHPFramework\Input;
+
+class JsonController
+{
+
+	protected Environment $twig;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+        $this->twig = $container->get(Environment::class);
+        $this->input = new Input();
+
+
+    }
+
+    public function __invoke($name, $params)
+    {
+
+        $reflection = new \ReflectionClass($this);
+
+
+        if ($reflection->hasMethod($name))
+		{
+			$method = $reflection->getMethod($name);
+		} else {
+
+        	die('there is no "' . $name . '" method in controller ' . get_class($this));
+		}
+
+        $values = [];
+
+        foreach ($method->getParameters() as $parameter) {
+
+            if ($parameter->getType()->isBuiltin()) {
+
+                if ( ! isset($params[$parameter->getName()]) && !$parameter->isDefaultValueAvailable()) {
+
+                    throw new \Exception('not set ' . $parameter->getName());
+                }
+
+                $values[] = $params[$parameter->getName()] ?? $parameter->getDefaultValue();
+
+            } else {
+
+                $values[] = $this->container->get($parameter->getClass()->name);
+            }
+
+        }
+
+        $res = call_user_func_array([$this, $name], $values);
+
+        header('Content-Type: application/json; charset=utf-8');
+        echo $res;
+
+    }
+
+    public function redirect($destination)
+    {
+
+        header("Location: " . $_SERVER['BASE_URL'] . $destination);
+        exit;
+
+    }
+
+    public function redirectToReferer()
+    {
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+        exit;
+
+    }
+
+
+}
