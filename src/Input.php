c1e33de6c1d3e8acbe42d0f5d0823bb945e9a7cf
Radek Stasiak <rad@MacBook-Air-Radek-2.local>
2022-04-07 22:02:01 +0200

    input

--- src/Input.php
+++ src/Input.php
@@ -15,70 +15,37 @@ class Input
         $this->get = $get ?? $_GET;
     }
 
-    public function post(string $name = '', bool $clean = true, string $type = 'string')
+    public function post(string $name = '')
     {
-        return $this->fetch('post', $name, $clean, $type);
+        return $this->fetch('post', $name);
     }
 
-    public function get(string $name = '', bool $clean = true, string $type = 'string')
+    public function get(string $name = '')
     {
-        return $this->fetch('get', $name, $clean, $type);
+        return $this->fetch('get', $name);
     }
 
-    private function load(string $type): array
+    private function fetch(string $type, string $name)
     {
 
-        return match ($type) {
-            'get' => $this->get,
+        $data = match ($type) {
             'post' => $this->post,
-            default => [],
+            default => $this->get
         };
-
-
-    }
-
-    private function fetch(string $type, string $name, bool $clean = true, string $valueType = 'string')
-    {
-        $data = $this->load($type);
-
-
-        if ($clean) {
-            $data = $this->clean($data);
-        }
 
         if ($name == '') {
             return $data;
         }
 
-        // jeśli podamy nazwę pola
+        if ( ! isset($data[$name]) || trim($data[$name]) === '') {
 
-        $rawValue = $data[$name] ?? null;
+            return null;
 
-        $value = match ($valueType) {
-            default => (string) $rawValue,
-            'bool' => (bool) $rawValue,
-            'int' => (int) $rawValue,
-        };
-
-        return $value ?? null;
-
-    }
-
-    private function clean(array $data): array
-    {
-        $cleaned = [];
-
-        foreach ($data as $key => $value) {
-            $cv = $this->cleanValue($value);
-            $cleaned[$key] = $cv;
         }
 
-        return $cleaned;
-    }
+        return $data[$name];
 
-    private function cleanValue(string $value): string
-    {
-        return htmlspecialchars($value, ENT_QUOTES, "UTF-8");
+
     }
 
 }
