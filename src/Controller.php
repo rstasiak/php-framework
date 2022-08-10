c8e603390022269be631551f588d0309a3afcd16
Radek Stasiak <rad@MacBook-Air-Radek-2.local>
2022-08-10 20:28:29 +0200

    logger

--- src/Controller.php
+++ src/Controller.php
@@ -4,15 +4,22 @@
 namespace PHPFramework;
 
 use DI\Container;
+use Monolog\Logger;
 use Twig\Environment;
 
 class Controller extends BaseController
 {
 
+    /**
+     * @var mixed|\Monolog\Logger
+     */
+    protected mixed $log;
+
     public function __construct(Container $container)
     {
         parent::__construct($container);
         $this->twig = $container->get(Environment::class);
+        $this->log = $container->get(Logger::class);
         $this->input = new Input();
 
 
