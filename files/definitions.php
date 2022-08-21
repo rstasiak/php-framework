ad89b1f6a0cf68917da973afca732b468473765e
Radek Stasiak <rad@MacBook-Air-Radek-2.local>
2022-08-21 11:24:07 +0200

    bulma

--- files/definitions.php
+++ files/definitions.php
@@ -1,8 +1,11 @@
 <?php
 
 
+use Monolog\Logger;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\ErrorLogHandler;
 
 return [
 
@@ -17,6 +20,19 @@ return [
         ]);
 
 	},
+
+    Logger::class => function() {
+
+        $logger = new Logger('app');
+
+        $logger->pushHandler(new StreamHandler(ROOT_DIR."/logs/info.log", Logger::INFO, true));
+        $logger->pushHandler(new StreamHandler(ROOT_DIR."/logs/debug.log", Logger::DEBUG, true));
+        $logger->pushHandler(new StreamHandler(ROOT_DIR."/logs/critical.log", Logger::CRITICAL, true));
+        $logger->pushHandler(new ErrorLogHandler(0, Logger::INFO, true));
+
+        return $logger;
+
+    },
 
 
 ];
